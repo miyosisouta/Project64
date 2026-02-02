ae39cb3a4fb73404cd64e78d998773d9365b0345
miyoshiSota <CA01244028@st.kawahara.ac.jp>
2026-02-02 17:23:08 +0900

    エフェクトマネージャーとタイプを追加、mainクラスにてエフェクトマネージャーとサウンドマネージャーを生成・削除

--- GameTemplate/Game/stdafx.h
+++ GameTemplate/Game/stdafx.h
@@ -4,3 +4,8 @@
 #include "k2EnginePreCompile.h"
 using namespace nsK2EngineLow;
 using namespace nsK2Engine;
+
+/* ここにインクルードしたクラスはstdtx.hをインクルードしたすべてのクラスで使えるようになる */
+/* 汎用クラスの追加 */
+#include "Sound/SoundManager.h"
+#include "Effect/GameEffectManager.h"
